9a173ca6853f47db7d17e87b8c5195f35c400a1c
IzumiSy <beetle-noise@gmx.com>
2022-01-05 13:26:54 +0900

    Use fdkaac

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module rtmp-server
 
 go 1.17
 
-require github.com/nareix/joy4 v0.0.0-20200507095837-05a4ffbb5369 // indirect
+require (
+	github.com/IzumiSy/go-fdkaac v0.0.0-20211230112620-fdfce1c7637d
+	github.com/google/uuid v1.3.0
+	github.com/nareix/joy4 v0.0.0-20200507095837-05a4ffbb5369
+)
--- main.go
+++ main.go
@@ -1,5 +1,70 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/IzumiSy/go-fdkaac"
+	uuid "github.com/google/uuid"
+	"github.com/nareix/joy4/format/rtmp"
+)
+
 func main() {
-	println("hello")
+	server := &rtmp.Server{}
+	server.HandlePublish = func(conn *rtmp.Conn) {
+		defer func() {
+			conn.Close()
+			log.Println("Connection closed")
+		}()
+
+		log.Println("HandlePublish")
+
+		id := uuid.New()
+		log.Printf("ID: %s", id)
+
+		file, err := os.Create(fmt.Sprintf("result-%s.pcm", id))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		// AAC LC/48000Hz/monoralなASCの設定でfdk-aacのデコーダを初期化
+		decoder := fdkaac.NewAacDecoder()
+		if err := decoder.InitRaw([]byte{0x11, 0x88}); err != nil {
+			panic(err)
+		}
+		defer decoder.Close()
+
+		// Read packets
+		pcmBuffer := new(bytes.Buffer)
+		pcmBuffer.Grow(256)
+		for {
+			if pkt, err := conn.ReadPacket(); err != nil {
+				if err == io.EOF {
+					break
+				}
+				return
+			} else {
+				pcmBuffer.Reset()
+				if err := decoder.Decode(pkt.Data, pcmBuffer); err != nil {
+					log.Fatal(err)
+				}
+
+				written, err := file.Write(pcmBuffer.Bytes())
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				log.Printf("Chunk: %d --> PCM %d", len(pkt.Data), written)
+			}
+		}
+
+		log.Println("Done")
+	}
+
+	log.Println("Server running...")
+	log.Fatal(server.ListenAndServe())
 }
